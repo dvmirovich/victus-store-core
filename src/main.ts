0761505340a87abb757dd6313856249ddb759741
rabdukadyrov <rabdukadyrov@nurtelecom.kg>
2025-08-18 09:47:06 +0600

    configured the arch

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './application/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
+  logger.log('Application is starting in port ' + (process.env.PORT ?? 3000));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
